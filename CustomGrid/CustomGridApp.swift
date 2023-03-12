3595068d54e893b5bc3530dba059b781a683f572
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-03-12 19:47:43 +0200

    Initial Commit

---
+++ CustomGrid/CustomGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomGridApp.swift
+//  CustomGrid
+//
+//  Created by Vladimir Dvornikov on 12/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
